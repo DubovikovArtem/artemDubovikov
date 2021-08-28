95fdbaf41339bb00456136cffd69d464e193c241
Dubovikov Artem <dubovikov.arm@gmail.com>
2021-08-28 13:49:45 +0300

    Branch main, Homework for lesson 20:
     update package: lesson18
     update class: TestCheckGps: -> add new object WebDriverWait wait

--- com.hillel/src/test/java/lesson18/TestCheckGps.java
+++ com.hillel/src/test/java/lesson18/TestCheckGps.java
@@ -1,16 +1,19 @@
 package lesson18;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class TestCheckGps extends BaseTest{
     @Test
     public void testGetGeoLocation() throws InterruptedException {
         driver.get("https://the-internet.herokuapp.com/geolocation");
+        WebDriverWait wait = new WebDriverWait(driver, 10000);
         driver.findElement(By.className("example")).findElement(By.tagName("button")).click();
-        Thread.sleep(1000);
 
         driver.findElement(By.id("demo"));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("lat-value")));
         String latValue = driver.findElement(By.id("lat-value")).getText();
         String longValue = driver.findElement(By.id("long-value")).getText();
 
